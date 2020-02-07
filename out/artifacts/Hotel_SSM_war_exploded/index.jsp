4c83fa75a4e2b654e2b89547e3aa011c1fe5c1f8
unknown <1192433075@qq.com>
2020-02-07 15:47:39 +0800

    first commit

---
+++ out/artifacts/Hotel_SSM_war_exploded/index.jsp
@@ -0,0 +1,5 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+  //跳转到后台主页
+  response.sendRedirect("home/index");
+%>
---
+++ src/com/zwf/dao/admin/LogDao.java
@@ -0,0 +1,22 @@
+package com.zwf.dao.admin;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.zwf.entity.admin.Authority;
+import com.zwf.entity.admin.Log;
+
+/**
+ * 系统日志类dao
+ * @author llq
+ *
+ */
+@Repository
+public interface LogDao {
+	public int add(Log log);
+	public List<Log> findList(Map<String, Object> queryMap);
+	public int getTotal(Map<String, Object> queryMap);
+	public int delete(String ids);
+}
